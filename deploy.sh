74824e77d013ec8209d672f1201f29233346109e
Carlo Gilmar <carlogilmar12@gmail.com>
2022-08-24 12:52:33 -0500

    New Release, repo principal

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,9 @@ echo "2. Guardando cambios en public/"
 cd public
 git add .
 git commit -m "New Release, repo GitHub Pages"
+git push origin main
 echo "3. Guardando cambios en repo principal"
 cd ..
 git add .
 git commit -m "New Release, repo principal"
+git push origin master
