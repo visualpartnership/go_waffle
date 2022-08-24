00407a222571acdd2ded5e0b1ab4689fc21bfafc
Carlo Gilmar <carlogilmar12@gmail.com>
2022-08-24 13:26:18 -0500

    Adding changes

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 echo "::Actualizando blog::"
-echo "0. Eliminando public/"
-rm -rf public/
 echo "1. Generando sitio... hugo -D"
 hugo -D
 echo "2. Guardando cambios en public/"
 cd public
+git checkout main
 git add .
 git commit -m "New Release, repo GitHub Pages"
 git push origin main
